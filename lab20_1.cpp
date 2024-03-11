12679946f3a410b8367a7c86904c99be7ba63d5a
Natwaa <Natwara_ch@cmu.ac.th>
2024-03-11 15:35:31 +0700

    lab20 completed ลืมลงGit

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -20,20 +20,64 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename, vector<string> &names, vector<int> &scores, vector<char> &grades){
+    ifstream fin(filename.c_str());
+    string textline;
+    char format[] = "%[^:]: %d %d %d";
+    int a,b,c;
+    char name[100];
+    while(getline(fin, textline)){
+        const char * textlineConverted = textline.c_str();
+        sscanf(textlineConverted, format, name, &a, &b, &c);
+        names.push_back(name);
+        scores.push_back(a+b+c);
+        grades.push_back(score2grade(a+b+c));
+    }
+    fin.close();
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &key){
+    cout << "Please input your command: " ;
+    char format[] = "%s %[^\n]";
+    char d[100];
+    char s[100];
+    string text;
+    getline(cin,text);
+    sscanf(text.c_str(),format,d,s);
+    command = d;
+    key = s;
 }
 
-void searchName(){
-
+void searchName(vector<string> names,vector<int> scores,vector<char> grades,string key){
+    cout << "---------------------------------\n";
+    for (unsigned int i=0;i<names.size();i++) {
+        if (toUpperStr(names[i]) == key) {
+            cout << names[i] << "'s score = " << scores[i] << "\n";
+            cout << names[i] << "'s grade = " << grades[i] << "\n";
+            cout << "---------------------------------\n";
+            return;
+        }
+    }
+    cout << "Cannot found." << "\n";
+    cout << "---------------------------------\n"; 
 }
 
-void searchGrade(){
+void searchGrade(vector<string> names,vector<int> scores,vector<char> grade,string key){
+    unsigned int check=0;
+    cout<<"---------------------------------\n";
+    for(unsigned int i=0;i<scores.size();i++){
+        char keyy=key[0];
+        if(keyy == grade[i]){
+         printf("%s (%d)\n",names[i].c_str(),scores[i]);
+        }else{
+            check++;
+        }
+    }
 
+    if(check == scores.size()){
+        cout<<"Cannot found.\n";
+    }
+    cout<<"---------------------------------\n";
 }
 
 
